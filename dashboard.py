2ce1f8853ba0ec2e70cc5a041c53479fbdb79098
Rayan20 <raymondytang76@gmail.com>
2022-10-15 15:40:01 -0500

    The Dark Knight Rises

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
 import matplotlib.pyplot as plt
-import os
 import plotly.graph_objects as go
-
-BASE_DIR = os.getcwd()
-
-#img = Image.open(os.path.join(BASE_DIR, 'images', 'ENGIE-Logo-Gradient-Blue-Full.png'))
-# st.image(os.path.join(BASE_DIR, 'images', 'ENGIE-Logo-Gradient-Blue-Full.png'), width=200)
+import get_realtime_data as grt
 
 st.set_page_config(
     page_title="Carbon Footprint Data Visualization",
@@ -19,40 +13,35 @@ st.set_page_config(
 
 st.title("Carbon Footprint Emission Visualization")
 
-# Streamlit Dashboard
 st.write("1-Year Historical Emissions")
-# We need data here from level 1
 
 st.write("Real-Time Emission Dashboard")
 
-# labels = ['Renewable', 'Coal', 'Natural Gas', 'Nuclear', 'Other']
-# sizes = [15, 30, 45, 10, 10]
-# # pie chart
-# fig1, ax1 = plt.subplots()
-# ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-#         shadow=True, startangle=90)
-# ax1.axis('equal')
+data = grt.get_miso_electricity_data()
+sumEnergy = data['wind'] + data['solar'] + data['hydro'] + data['coal'] + data['natural_gas'] + data['nuclear'] + data['other']
+sumClean = data['wind'] + data['solar'] + data['hydro'] + data['nuclear']
+sumDirty = data['natural_gas'] + data['coal']
+sumOther = data['other']
 
-# st.pyplot(fig1)
+pc = dict()
+for key, val in data.items():
+    pc[key] = round(val/sumEnergy * 100)
+
+pcClean = pc['wind'] + pc['solar'] + pc['hydro'] + pc['nuclear']
+pcDirty = pc['natural_gas'] + pc['coal']
+
+print(pc)
+print(sum(pc.values()))
 
 st.slider("Select a year", 2019, 2020, 2020)
 
 fig =go.Figure(go.Sunburst(
     labels=["Grid Electricity", "Clean Energy", "Dirty Energy", "Coal", "Natural Gas", "Wind", "Nuclear", "Solar", "Hydro", "Other"],
-    parents=["", "Grid Electricity", "Grid Electricity", "Dirty Energy", "Dirty Energy", "Clean Energy", "Clean Energy", "Clean Energy", "Clean Energy", "Clean Energy"],
-    values=[0, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11],
+    parents=["", "Grid Electricity", "Grid Electricity", "Dirty Energy", "Dirty Energy", "Clean Energy", "Clean Energy", "Clean Energy", "Clean Energy", "Grid Electricity"],
+    branchvalues="total",
+    values=[sum(pc.values()), pcClean, pcDirty, pc['coal'], pc['natural_gas'], pc['wind'], pc['nuclear'], pc['solar'], pc['hydro'], pc['other']]
 ))
 fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
 fig.update_traces(textinfo="label+percent parent")
 
-
 st.plotly_chart(fig)
-
-
-
-'''
-while True:
-    # We need live data here
-    pass
-'''
-
